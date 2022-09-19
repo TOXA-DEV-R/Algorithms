bc348a8324c617bc0be747d4806c4991e097c0a0
Tohir <TOXIR_DEV>
2022-09-19 20:08:47 +0600

    reverseDigits

--- main.dart
+++ main.dart
@@ -1,1 +1,29 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  print(reverseDigits(num: 58954));
+}
+
+// void printDigits(int num) {
+//   if (num == 0) {
+//     print(0);
+//     return;
+//   }
+
+//   while (num != 0) {
+//     int digit = num % 10;
+//     print(digit);
+//     num = (num / 10).floor();
+//   }
+// }
+
+int reverseDigits({required int num}) {
+  int result = 0;
+
+  while (num != 0) {
+    result *= 10;
+    int digit = num % 10;
+    result += digit;
+    num = (num / 10).floor();
+  }
+
+  return result;
+}
