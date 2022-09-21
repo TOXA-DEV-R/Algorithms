d16f699da5ba9e0511e6d783ad67c938ac3107de
Tohir <TOXIR_DEV>
2022-09-21 20:17:21 +0600

    bitwise operator

--- main.dart
+++ main.dart
@@ -1,6 +1,42 @@
 void main(List<String> args) {
-  print(reverseDigits(num: 58954));
+  // print(1100 & 1001);
 }
+
+// bool bitwise_ConvertToBinary(int num) {
+//   int bitCount = 0;
+
+//   while (num != 0) {
+//     bitCount += num & 1;
+//     num = num >> 1;
+//   }
+
+//   return bitCount == 1;
+// }
+
+// String convertToBinary(int num) {
+//   StringBuffer result = new StringBuffer('');
+
+//   while (num != 0) {
+//     int digit = num % 2;
+//     result.write('$digit');
+//     num = (num / 2).floor();
+//   }
+
+//   return result.toString().split('').reversed.toList().join();
+// }
+
+// int reverseDigits({required int num}) {
+//   int result = 0;
+
+//   while (num != 0) {
+//     result *= 10;
+//     int digit = num % 10;
+//     result += digit;
+//     num = (num / 10).floor();
+//   }
+
+//   return result;
+// }
 
 // void printDigits(int num) {
 //   if (num == 0) {
@@ -9,21 +45,8 @@ void main(List<String> args) {
 //   }
 
 //   while (num != 0) {
-//     int digit = num % 10;
-//     print(digit);
+//     print(num % 10);
 //     num = (num / 10).floor();
 //   }
 // }
 
-int reverseDigits({required int num}) {
-  int result = 0;
-
-  while (num != 0) {
-    result *= 10;
-    int digit = num % 10;
-    result += digit;
-    num = (num / 10).floor();
-  }
-
-  return result;
-}
