637af90d5b77f47fe22542bce3e69196bf05ceee
Tohir <TOXIR_DEV>
2022-09-24 19:57:08 +0600

    Linked List

--- main.dart
+++ main.dart
@@ -1,6 +1,75 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final list = LinkedList<String>();
+
+  print(list.head?.value);
+}
 
 /*Linked List*/
+
+class Node<T> {
+  Node({required this.value, this.next});
+
+  T value;
+  Node<T>? next;
+}
+
+class LinkedList<E> {
+  Node<E>? head;
+  Node<E>? tail;
+
+  bool get isEmpty => head == null;
+
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  void append(E value) {
+    // 1
+    if (isEmpty) {
+      push(value);
+      return;
+    }
+
+    // 2
+    tail!.next = Node(value: value);
+
+    // 3
+    tail = tail!.next;
+  }
+
+  Node<E>? nodeAt(int index) {
+    // 1
+    var currentNode = head;
+    var currentIndex = 0;
+
+    // 2
+    while (currentNode != null && currentIndex < index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    return currentNode;
+  }
+
+  Node<E> insertAfter(Node<E> node, E value) {
+    // 1
+    if (tail == node) {
+      append(value);
+      return tail!;
+    }
+
+    // 2
+    node.next = Node(value: value, next: node.next);
+    return node.next!;
+  }
+
+  @override
+  String toString() {
+    if (isEmpty) return 'Empty list';
+    return head.toString();
+  }
+}
 
 // List<int> mergeTwoArrays(
 //     {required List<int> array1, required List<int> array2}) {
